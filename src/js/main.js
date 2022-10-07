fdf06db92aa18fd249265ea814e529e21d2da22e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2022-10-07 13:05:04 +0800

    Added the js functionalities

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 const nav = document.querySelector('.header__navigation');
 
 const navToggler = document.querySelector('.header__nav__toggler');
+
+navToggler.addEventListener('click', () => {
+  const visibility = nav.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    nav.setAttribute('data-visible', true);
+    navToggler.setAttribute('aria-expanded', true);
+  } else {
+    nav.setAttribute('data-sisible', false);
+    navToggler.setAttribute('aria-expanded', false);
+  }
+})
